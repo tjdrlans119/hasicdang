4a4d83936443c7a764eb083510a5a020b0e85d33
kimoonss <tjdrlans119@gmail.com>
2020-05-03 23:38:29 +0900

    clean: site routes

--- src/index.js
+++ src/index.js
@@ -35,22 +35,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Switch>
-        <Route path="/index" render={props => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route path="/login-page" render={props => <LoginPage {...props} />} />
-        <Redirect to="/index" />
-        <Redirect from="/" to="/index" />
+        <Route path="/" render={props => <Index {...props} />} />
+        <Redirect from="/index" to="/" />
       </Switch>
     </Switch>
   </BrowserRouter>,
