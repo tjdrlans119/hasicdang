3ff14b2d3e1f7c2faef3cbacda406190a4c2f0f4
kimoonss <tjdrlans119@gmail.com>
2020-05-06 02:22:22 +0900

    edit address & map style change

--- src/views/index-sections/Address.js
+++ src/views/index-sections/Address.js
@@ -2,14 +2,6 @@ import React from "react";
 // reactstrap components
 import { Button, Container, Row, Col } from "reactstrap";
 import MapContent from "../index-sections/MapContent"
-// core components
-
-// const Style = {
-//   map:{
-//     width:"%",
-//     height:"500px"
-//   }
-// }
 
 function Address() {
   return (
@@ -18,26 +10,24 @@ function Address() {
         className="section section-address"
         data-background-color="black"
         id="address-section"
-        style={{margin:'0 auto',  textAlign:'center'}}
+        style={{ margin: '0 auto', textAlign: 'center' }}
       >
         <Container>
           <h2 className="title">찾아오시는길</h2>
-          <Row>
-            <Col lg="3" md="12">
+          <Row gutter={16}>
+            <Col lg="6" md="6">
+              <MapContent
+                mapTarget={"first"}
+              />
               <h5 className="description">
-                안양 1호점
+                안양점
+                <br />
+                ( 경기 안양시 만안구 만안로 214-2, 2층 )
               </h5>
-              <div>
-               <MapContent
-               mapTarget={"first"}
-               />
-              </div>
-              </Col>
-              <Col>
               <Button
                 className="btn-round mr-1"
                 color="info"
-                href="https://map.kakao.com/link/to/하식당,37.400046,126.923564"
+                href="https://map.kakao.com/link/to/하식당 안양점,37.400046,126.923564"
                 size="sm"
                 target="_blank"
               >
@@ -49,26 +39,24 @@ function Address() {
                 href="tel:01031577003"
                 outline
                 size="sm"
-              > 
+              >
                 연락하기
               </Button>
             </Col>
-            <Col lg="6" md="12">
+            <Col lg="6" md="6">
+              <MapContent
+                mapTarget={"second"}
+              />
               <h5 className="description">
-                범계 2호점
+                범계점
+                <br />
+                ( 경기 안양시 동안구 평촌대로223번길 19, 401호 )
               </h5>
-              <div>
-               <MapContent 
-               testProps={123}
-               mapTarget={"second"}
-              //  lat={37.389888}
-              //  lng={126.950616  }
-               />
-              </div>
+
               <Button
                 className="btn-round mr-1"
                 color="info"
-                href="https://map.kakao.com/link/to/하식당,37.389888,126.950616"
+                href="https://map.kakao.com/link/to/하식당 범계점,37.390884,126.954646"
                 size="sm"
                 target="_blank"
               >
--- src/views/index-sections/MapContent.js
+++ src/views/index-sections/MapContent.js
@@ -2,55 +2,56 @@
 import React, { Component } from 'react';
 
 const Style = {
-  map:{
-    width:"80%",
-    height:"400px",
-    textAlign:'center',
-    margin:'0 auto'
+  map: {
+    width: "80%",
+    height: "300px",
+    textAlign: 'center',
+    margin: '0 auto',
+    marginTop: '50px',
+    borderRadius: "1em"
   }
 }
 
 const store = {
-  first:{
-    storeTitle:"안양1호점",
-    lat:37.400046,
-    lng:126.923564,
+  first: {
+    storeTitle: "안양점",
+    lat: 37.400046,
+    lng: 126.923564,
   },
-  second:{
-    storeTitle:"범계2호점",
-    lat:37.389888,
-    lng:126.950616,
+  second: {
+    storeTitle: "범계점",
+    lat: 37.390884,
+    lng: 126.954646,
   },
 }
 
-
 class MapContent extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
-    componentDidMount() {
-      const {mapTarget} = this.props
-      const script = document.createElement("script");
-      script.async = true;
-      script.src =`https://dapi.kakao.com/v2/maps/sdk.js?appkey=f9e8d61a7a2cab818654d22b5acc1129&autoload=false`
-      document.head.appendChild(script);
+  componentDidMount() {
+    const { mapTarget } = this.props
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=f9e8d61a7a2cab818654d22b5acc1129&autoload=false`
+    document.head.appendChild(script);
 
-      if(mapTarget){
+    if (mapTarget) {
       script.onload = () => {
         kakao.maps.load(() => {
-          
+
           let container = document.getElementById(mapTarget);
           let options = {
             center: new kakao.maps.LatLng(store[mapTarget].lat, store[mapTarget].lng),
-            level: 5
+            level: 4
           };
           const map = new window.kakao.maps.Map(container, options);
           // 마커가 표시될 위치입니다 
-          let markerPosition  = new kakao.maps.LatLng(store[mapTarget].lat, store[mapTarget].lng); 
+          let markerPosition = new kakao.maps.LatLng(store[mapTarget].lat, store[mapTarget].lng);
 
           // 마커를 생성합니다
           let marker = new kakao.maps.Marker({
-              position: markerPosition
+            position: markerPosition
           });
 
           // 마커가 지도 위에 표시되도록 설정합니다
@@ -61,13 +62,13 @@ class MapContent extends Component {
       };
     }
 
-    }
-    
-
-    render() {
-      const {mapTarget} = this.props
-      return <div id={mapTarget} style={Style.map}></div>; // 이부분이 지도를 띄우게 될 부분.
-    }
   }
+
+
+  render() {
+    const { mapTarget } = this.props
+    return <div id={mapTarget} style={Style.map}></div>; // 이부분이 지도를 띄우게 될 부분.
+  }
+}
 
 export default MapContent;
